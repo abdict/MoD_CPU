49a8f475a5c48b1efbb59b30a27b82237381d190
abdict <abdict@live.com>
2015-10-07 16:04:01 -0700

    Hello World 2nd push.

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -5,8 +5,24 @@
 
 using namespace std;
 
-int main(void)
+int main(int argc, char** argv)
 {
+	string arg_in;
+	int count = argc;
+
+	if (count >= 2)
+	{
+		arg_in.assign(argv[--count]);
+		if (arg_in == "-p")
+		{
+			cout << "There are only " << argc << " flags associated with current run." << endl;
+		}
+		else
+		{
+			cout << "the flag argument " << arg_in << " is not recognized!" << endl;
+		}
+	}
+
 	string out{ "Hello World !" };
 
 	cout << setw(16) << right << out << endl;
